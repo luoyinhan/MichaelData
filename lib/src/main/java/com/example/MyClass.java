9de139a4b06157cf5009f661d85ff627ae9ecfcc
Micheal <lyhwtu@126.com>
2016-11-30 20:20:19 +0800

    修改 测试库文件

--- lib/src/main/java/com/example/MyClass.java
+++ lib/src/main/java/com/example/MyClass.java
@@ -3,7 +3,6 @@ package com.example;
 public class MyClass {
 
     public static void main(String[] args) {
-        System.out.println(Integer.parseInt("12345", 16));
-//        System.out.println(Integer.toHexString(780));
+       
     }
 }
